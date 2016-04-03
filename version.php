30b178397bd94d05b9b261efed344777de94a988
lazydaisy <lazydaisy@visible-expression.co.uk>
2016-04-03 22:37:11 +0100

    Created MORECANDY_30_STABLE branch:

--- version.php
+++ version.php
@@ -26,10 +26,10 @@ defined('MOODLE_INTERNAL') || die;
 
 
 $plugin->component    = 'theme_morecandy';
-$plugin->maturity     = MATURITY_ALPHA;      // This version's maturity level.
-$plugin->release      = '3.1 (Build: 2016)'; // Human-friendly version name.
-$plugin->requires     = 2016032400;          // This version of Moodle.
-$plugin->version      = 2016040100;          // Latest build date. YYYYMMDD.
+$plugin->maturity     = MATURITY_STABLE;     // This version's maturity level.
+$plugin->release      = '3.0 (Build: 2016)'; // Human-friendly version name.
+$plugin->requires     = 2015111603.03;       // This version of Moodle.
+$plugin->version      = 2016040400;          // Latest build date. YYYYMMDD.
 $plugin->dependencies = array(               // The version of the parent theme.
     'theme_bootstrapbase'  => 2015111600,
 );
