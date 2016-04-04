cb376652fa9232667bcd1c9d962a5cdd8fbfedbd
lazydaisy <lazydaisy@visible-expression.co.uk>
2016-04-04 17:29:05 +0100

    Re-write and update to ongoing project.

--- classes/core_renderer.php
+++ classes/core_renderer.php
@@ -26,49 +26,31 @@ require_once($CFG->dirroot. '/theme/bootstrapbase/renderers.php');
 
 class theme_morecandy_core_renderer extends theme_bootstrapbase_core_renderer {
 
-    /*
-     * This renders the bootstrap top menu.
+    /**
+     * The standard tags (typically performance information and validation links,
+     * if we are in developer debug mode) that should be output in the footer area
+     * of the page. Designed to be called in theme layout.php files.
      *
-     * This renderer is needed to enable the Bootstrap style navigation.
+     * @return string HTML fragment.
      */
     protected function render_custom_menu(custom_menu $menu) {
-        global $CFG;
-
-        // TODO: eliminate this duplicated logic, it belongs in core, not
-        // here. See MDL-39565.
-        $addlangmenu = true;
-        $langs = get_string_manager()->get_list_of_translations();
-        if (count($langs) < 2
-            or empty($CFG->langmenu)
-            or ($this->page->course != SITEID and !empty($this->page->course->lang))) {
-            $addlangmenu = false;
-        }
-
-        if (!$menu->has_children() && $addlangmenu === false) {
-            return '';
-        }
-
-        if ($addlangmenu) {
-            $strlang = get_string('language');
-            $currentlang = current_language();
-            if (isset($langs[$currentlang])) {
-                $currentlang = $langs[$currentlang];
-            } else {
-                $currentlang = $strlang;
-            }
-            $this->language = $menu->add($currentlang, new moodle_url('#'), $strlang, 10000);
-            foreach ($langs as $langtype => $langname) {
-                $this->language->add($langname, new moodle_url($this->page->url, array('lang' => $langtype)), $langname);
-            }
-        }
-
         $content = '<ul class="nav">';
-        $content .= '<li class="first divider"></li>';
         foreach ($menu->get_children() as $item) {
             $content .= $this->render_custom_menu_item($item, 1);
         }
-        $content .= '<li class="last divider"></li>';
-        return $content.'</ul>';
+        $content .= '</ul>';
+        $patterns = array();
+        $replacements = array();
+
+        $patterns[0] = '/<ul class="nav">/';
+        $replacements[0] = '<ul class="nav"><li class="first divider"></li>';
+
+        $patterns[1] = '/<\/ul>/';
+        $replacements[1] = '<li class="last divider"></li></ul>';
+
+        $content = preg_replace($patterns, $replacements, $content);
+
+        return $content;
     }
 
     /**
@@ -87,173 +69,22 @@ class theme_morecandy_core_renderer extends theme_bootstrapbase_core_renderer {
         $replacements[0] = '<div class="performanceinfo pageinfo well"><i class="fa fa-cogs"></i>';
 
         $patterns[1] = '/<div class="purgecaches">(<a[^>]+>)([^<]+)<\/a>/';
-        $replacements[1] = '<div class="btn btn-default">${1}<i class="fa fa-trash"></i> ${2} </a>';
+        $replacements[1] = '<div class="purgecaches btn btn-default">${1}<i class="fa fa-trash"></i> ${2} </a>';
 
         $patterns[2] = '/<li><a([^>]+)>([^<]+)<\/a>/';
-        $replacements[2] = '<li><a class="btn btn-small btn-default"${1}><i class="fa fa-cogs"></i>&nbsp;&nbsp;${2}</a>';
+        $replacements[2] = '<li><a class="btn btn-small btn-default">${1}<i class="fa fa-cogs"></i>${2}</a>';
         $output = preg_replace($patterns, $replacements, $output);
 
         return $output;
     }
 
-    /**
-     * Return the 'back' link that normally appears in the footer.
-     *
-     * @return string HTML fragment.
-     */
-    public function home_link() {
-        global $CFG, $SITE;
-
-        if ($this->page->pagetype == 'site-index') {
-            // Special case for site home page - please do not remove.
-            return '';
-
-        } else if (!empty($CFG->target_release) && $CFG->target_release != $CFG->release) {
-            // Special case for during install/upgrade.
-            return html_writer::start_tag('div', array('class' => 'sitelink')) .
-                   html_writer::link('http://docs.moodle.org/en/Administrator_documentation',
-                   html_writer::empty_tag('img',
-                   array('style' => 'width:100px;height:30px;', 'src' => $this->pix_url('moodlelogo'), 'alt' => 'moodlelogo')),
-                   array('title' => 'Moodle', 'onclick' => 'this.target=_blank')).
-                   html_writer::end_tag('div');
-
-        } else if ($this->page->course->id == $SITE->id || strpos($this->page->pagetype, 'course-view') === 0) {
-            return html_writer::tag('div',
-                   html_writer::link($CFG->wwwroot,
-                   html_writer::tag('i', '', array('class' => 'icon-home')) . '&nbsp;' . get_string('home') .
-                   array('class' => 'btn btn-small')),
-                   array('class' => 'homelink'));
-        } else {
-            return html_writer::tag('div',
-                   html_writer::link($CFG->wwwroot . '/course/view.php?id=' . $this->page->course->id ,
-                   html_writer::tag('i', '', array('class' => 'icon-home')). '&nbsp;' .
-                   format_string($this->page->course->shortname, true, array('context' => $this->page->context)) .
-                   array('class' => 'btn btn-small')),
-                   array('class' => 'homelink'));
-        }
-    }
-
-    /**
-     * Return the standard string that says whether you are logged in (and switched
-     * roles/logged in as another user).
-     * @param bool $withlinks if false, then don't include any links in the HTML produced.
-     * If not set, the default is the nologinlinks option from the theme config.php file,
-     * and if that is not set, then links are included.
-     * @return string HTML fragment.
-     */
-    public function login_info($withlinks = null) {
-        global $USER, $CFG, $DB, $SESSION;
-
-        if (during_initial_install()) {
-            return '';
-        }
-
-        if (is_null($withlinks)) {
-            $withlinks = empty($this->page->layout_options['nologinlinks']);
-        }
-
-        $loginpage = ((string)$this->page->url === get_login_url());
-        $course = $this->page->course;
-        if (\core\session\manager::is_loggedinas()) {
-            $realuser = session_get_realuser();
-            $fullname = fullname($realuser, true);
-            if ($withlinks) {
-                $realuserinfo = " [<a href=\"$CFG->wwwroot/course/loginas.php?id=$course->id&amp;sesskey=".
-                sesskey() ."\">$fullname</a>] ";
-            } else {
-                $realuserinfo = " [$fullname] ";
-            }
-        } else {
-            $realuserinfo = '';
-        }
-
-        $loginurl = get_login_url();
-
-        if (empty($course->id)) {
-            // The $course->id is not defined during installation.
-            return '';
-        } else if (isloggedin()) {
-            $context = context_course::instance($course->id);
-
-            $fullname = fullname($USER, true);
-            // Since Moodle 2.0 this link always goes to the public profile page (not the course profile page).
-            if ($withlinks) {
-                $username = "<a href=\"$CFG->wwwroot/user/profile.php?id=$USER->id\">$fullname</a>";
-            } else {
-                $username = $fullname;
-            }
-            if (is_mnet_remote_user($USER) and $idprovider = $DB->get_record('mnet_host', array('id' => $USER->mnethostid))) {
-                if ($withlinks) {
-                    $username .= " from <a href=\"{$idprovider->wwwroot}\">{$idprovider->name}</a>";
-                } else {
-                    $username .= " from {$idprovider->name}";
-                }
-            }
-            if (isguestuser()) {
-                $loggedinas = $realuserinfo.get_string('loggedinasguest');
-                if (!$loginpage && $withlinks) {
-                    $loggedinas .= '<a class="btn btn-small btn-default" href="' . $loginurl . '">
-                    <i class="fa fa-sign-in"></i> ' . get_string('login') . '</a>';
-                }
-            } else if (is_role_switched($course->id)) { // Has switched roles.
-                $rolename = '';
-                if ($role = $DB->get_record('role', array('id' => $USER->access['rsw'][$context->path]))) {
-                    $rolename = ': '.format_string($role->name);
-                }
-                $loggedinas = get_string('loggedinas', 'moodle', $username).$rolename;
-                if ($withlinks) {
-                    $loggedinas .= ' (<a href="$CFG->wwwroot/course/view.php?id=$course->id&amp;switchrole=0&amp;sesskey='.
-                    sesskey().'">' . get_string('switchrolereturn') . '</a>)';
-                }
-            } else {
-                $loggedinas = $realuserinfo.get_string('loggedinas', 'moodle', $username);
-                if ($withlinks) {
-                    $loggedinas .= '&nbsp;&nbsp;<a class="btn btn-small" href="' .
-                    $CFG->wwwroot . '/login/logout.php?sesskey=' . sesskey() .
-                    '"><i class="fa fa-sign-out"></i> ' . get_string('logout') . '</a>';
-                }
-            }
-        } else {
-            $loggedinas = get_string('loggedinnot', 'moodle');
-            if (!$loginpage && $withlinks) {
-                $loggedinas .= ' <a class="btn btn-small btn-default" href="' .
-                $loginurl . '"><i class="fa fa-sign-in"></i> '.get_string('login').'</a>';
-            }
-        }
-
-        $loggedinas = '<div class="logininfo">'.$loggedinas.'</div>';
-
-        if (isset($SESSION->justloggedin)) {
-            unset($SESSION->justloggedin);
-            if (!empty($CFG->displayloginfailures)) {
-                if (!isguestuser()) {
-                    // Include this file only when required.
-                    require_once($CFG->dirroot . '/user/lib.php');
-                    if ($count = user_count_login_failures($USER)) {
-                        $loggedinas .= '<div class="loginfailures">';
-                        $a = new stdClass();
-                        $a->attempts = $count;
-                        $loggedinas .= get_string('failedloginattempts', '', $a);
-                        if (file_exists("$CFG->dirroot/report/log/index.php")
-                            and has_capability('report/log:view', context_system::instance())) {
-                            $loggedinas .= ' ('.html_writer::link(new moodle_url('/report/log/index.php',
-                            array('chooselog' => 1, 'id' => 0 , 'modid' => 'site_errors')), get_string('logs')).')';
-                        }
-                        $loggedinas .= '</div>';
-                    }
-                }
-            }
-        }
-
-        return $loggedinas;
-    }
 
     /**
      * Returns HTML to display a "Turn editing on/off" button in a form.
      *
      * @param moodle_url $url The URL + params to send through when clicking the button
      * @return string HTML the button
-     * written for Tiny Bootstrap Project byLazyDaisy.co.uk
+     * @copyright 2016 // Originally written for Tiny Bootstrap Project byLazyDaisy.co.uk.
      */
     public function edit_button(moodle_url $url) {
 
@@ -261,17 +92,17 @@ class theme_morecandy_core_renderer extends theme_bootstrapbase_core_renderer {
         if ($this->page->user_is_editing()) {
             $url->param('edit', 'off');
             $icon = 'fa-power-off';
-            $alt = get_string('turneditingoff');
+            $title = get_string('turneditingoff');
         } else {
             $url->param('edit', 'on');
             $icon = 'fa-edit';
-            $alt = get_string('turneditingon');
+            $title = get_string('turneditingon');
         }
 
+        $itag = html_writer::tag('i', '', array('class' => 'course-edit-icon fa '. $icon . ' fa-2x'));
         $content = '';
-        $content .= html_writer::link($url,
-                                  html_writer::tag('i', '', array('class' => 'course-edit-icon fa '. $icon . ' fa-2x')),
-                                  array('href' => $url, 'title' => $alt, 'alt' => $alt));
+
+        $content .= html_writer::link($url, $itag, array('href' => $url, 'title' => $title));
 
         return $content;
 
